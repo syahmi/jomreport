289195d2bc6d2c0e800c409f3c982baabae8827e
Syahmi Ismail <msi@syamey.com>
2015-11-11 21:00:14 +0800

    Add SettingsVC

---
+++ JomReport/JomReport/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  JomReport
+//
+//  Created by MSi on 11/11/2015.
+//  Copyright © 2015 JomReport. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
