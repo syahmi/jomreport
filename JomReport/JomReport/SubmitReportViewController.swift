e7767e09830e2d34e8a4fbe0afbb17e6341a5373
aminalzanki <nik.muhammad.amin@gmail.com>
2015-11-21 10:57:14 +0800

    Create a submit report view controller

---
+++ JomReport/JomReport/SubmitReportViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SubmitReportViewController.swift
+//  JomReport
+//
+//  Created by Nik Muhammad Amin on 11/11/15.
+//  Copyright © 2015 JomReport. All rights reserved.
+//
+
+import UIKit
+
+class SubmitReportViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
